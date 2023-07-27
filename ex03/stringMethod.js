0937173eac67b50a07dae1f0097792564570224e
heejinkong <gmlwls3379@naver.com>
2023-07-27 14:35:51 +0900

    부분 문자열 추출하기
    부분 문자열 추출

---
+++ ex03/stringMethod.js
@@ -0,0 +1,12 @@
+let str = "stringfy";
+
+console.log( str.slice(0, 5) ); 
+console.log( str.slice(0, 1) );
+console.log( str.slice(2) );
+console.log( str.slice(-4, -1) ); 
+
+console.log( str.substring(2, 6) );
+console.log( str.substring(6, 2) );
+console.log( str.slice(2, 6) );
+console.log( str.slice(6, 2) );
+
